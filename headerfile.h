2d6be7b94df9b8aabf8b4838a2eebd7c9ec58a37
dudn025 <dudn025@naver.com>
2017-11-27 02:27:09 +0900

    업데이트headerfile.h

--- headerfile.h
+++ headerfile.h
@@ -1,11 +1,17 @@
 // file: headerfile.h
-#define _CRT_SECURE_NO_WARNINGS //scanf(), gets()등의 오류를 방지하기 위한 상수 정의
+#define _CRT_SECURE_NO_WARNINGS 
+//scanf(), gets()등의 오류를 방지하기 위한 상수 정의
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
-struct Node	//Linked List를 구현할 수 있는 구조체
+struct node	//Linked List를 구현할 수 있는 구조체
 {
 	int value;			//값
-	struct Node *next;	//next value pointer
+	struct node *next;	//next value pointer
 };
+
+typedef struct node NODE;
+typedef NODE *LINK;
+
+int cnt;
