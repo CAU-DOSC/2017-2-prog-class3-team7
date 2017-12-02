0f2c9581b2b1ab305005d0e96ca25caa6e0b795d
galaxy821 <hoshion208@naver.com>
2017-12-03 06:16:09 +0900

    add reverse.c

---
+++ Rotation/reverse.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void reverse(char *str1, int d, int length)
+{
+	char *rev_str_A = (char*)malloc(d + 1);
+	char *rev_str_B = (char*)malloc(length-d+1);
+	int j = length - d;
+	if (d > 0)
+	{
+		for (int i = 0; i < d; i++)
+		{
+			{
+				for (int i = 0; i < d; i++)
+					rev_str_A[d-i-1] = str1[i];
+
+				for (int i = d; i < length; i++)
+				{
+					rev_str_B[j-1] = str1[i];
+					j--;
+				}
+			}
+		}
+		return;
+	}
+	else if (d < 0)
+	{
+		for (int i = 0; i < d; i++)
+		{
+			
+		}
+		return;
+	}
+}
