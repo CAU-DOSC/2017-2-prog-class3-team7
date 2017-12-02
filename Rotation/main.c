7f721442a3b6890c65babb67d48cdaf1994df754
galaxy821 <hoshion208@naver.com>
2017-12-03 05:04:35 +0900

    add source code for main.c

---
+++ Rotation/main.c
@@ -0,0 +1,40 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main()
+{
+	int length;
+	int rotate;
+	int d;
+
+	double trivial_time;
+	double BlockSwap_time;
+	clock_t start, end;
+
+	printf("input str1 length>");
+	scanf("%d", &length);
+	printf("input rotate distance>");
+	scanf("%d", &rotate);
+	printf("select number\n");
+	printf("input positive number : clockwise rotation\n");
+	printf("input negative number : countclockwise rotation\n>");
+	scanf("%d", &d);
+	char *str1 = (char *)malloc(length + 1);
+	for (int i = 0; i < length; i++)
+	{
+		str1[i] = i;
+	}
+	start = clock();
+	trivial(str1, rotate, d, length);
+	end = clock();
+	trivial_time = end - start;
+
+	start = clock();
+	BlockSwap(str1, rotate, d, length);
+	end = clock();
+	BlockSwap_time = end - start;
+
+	return 0;
+}
