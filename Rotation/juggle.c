cb0a5fa22f5fa352fec5d81f3d65046d35f1af6f
galaxy821 <hoshion208@naver.com>
2017-12-03 05:35:56 +0900

    add juggle.c

---
+++ Rotation/juggle.c
@@ -0,0 +1,37 @@
+
+void juggle(char *str1, int d, int length)
+{
+	char* juggle = (char *)malloc(length + 1);
+
+	if (d > 0)
+	{
+		for (int i = 0; i < length; i++)
+		{
+			if (i > d - 1)
+				juggle[i - d] = str1[i];
+			else
+				juggle[i + (length - d)] = str1[i];
+		}
+		for (int i = 0; i < length; i++)
+		{
+			str1[i] = juggle[i];
+		}
+		return;
+	}
+	else if (d < 0)
+	{
+		for (int i = 0; i < length; i++)
+		{
+			if (i < (length - d))
+				juggle[i + d] = str1[i];
+			else
+				juggle[i - (length - d)] = str1[i];
+		}
+		for (int i = 0; i < length; i++)
+		{
+			str1[i] = juggle[i];
+		}
+		return;
+	}
+
+}
