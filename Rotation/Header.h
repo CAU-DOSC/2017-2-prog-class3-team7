d6633ea34b4e15502bf81afb37f14670089e3ab2
JungYoon Lee <qoo0311@naver.com>
2017-12-03 20:13:55 +0900

    Header.h

---
+++ Rotation/Header.h
@@ -0,0 +1,11 @@
+// file: Header.h
+
+#define _CRT_SECUE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+void trivial(char *str1, int length, int d);
+void reverse(char *str1, int d, int length);
+void juggle (char *str, int d, int length);
+void BlockSwap(char *str, int n, int d);
