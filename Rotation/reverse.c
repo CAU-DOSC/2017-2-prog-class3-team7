82fbd6fcd36f95dc71fe9369049e35878490d16c
DogRing <solo_monster@naver.com>
2017-12-05 02:49:20 +0900

    modified reverse.c

--- Rotation/reverse.c
+++ Rotation/reverse.c
@@ -3,32 +3,40 @@
 
 void reverse(char *str1, int length, int d)
 {
-	char *rev_str_A = (char*)malloc(d + 1);
-	char *rev_str_B = (char*)malloc(length-d+1);
-	int j = length - d;
-	if (d > 0)
-	{
-		for (int i = 0; i < d; i++)
-		{
-			{
-				for (int i = 0; i < d; i++)
-					rev_str_A[d-i-1] = str1[i];
-
-				for (int i = d; i < length; i++)
-				{
-					rev_str_B[j-1] = str1[i];
-					j--;
-				}
-			}
+	char temp;
+	if (d > 0) {											// if d > 0
+		for (int i = 0;i < d/2;i++) {						// reverse A
+			temp = str1[i];
+			str1[i] = str1[d - 1 - i];
+			str1[d - 1 - i] = temp;
 		}
-		return;
+		for (int i = 0;i < (length - d)/2;i++) {			// reverse B
+			temp = str1[d+i];
+			str1[d + i] = str1[length - 1 - i];
+			str1[length - 1 - i] = temp;
+		}
+		for (int i = 0;i < length / 2;i++) {				// reverse ArBr
+			temp = str1[i];
+			str1[i] = str1[length - 1 - i];
+			str1[length - 1 - i] = temp;
+		}
 	}
-	else if (d < 0)
-	{
-		for (int i = 0; i < d; i++)
-		{
-			
+	else {													// if d<=0
+		d = -d;
+		for (int i = 0;i < length / 2;i++) {				// reverse ArBr
+			temp = str1[i];
+			str1[i] = str1[length - 1 - i];
+			str1[length - 1 - i] = temp;
 		}
-		return;
+		for (int i = 0;i < d / 2;i++) {						// reverse A
+			temp = str1[i];
+			str1[i] = str1[d - 1 - i];
+			str1[d - 1 - i] = temp;
+		}
+		for (int i = 0;i < (length - d) / 2;i++) {			// reverse B
+			temp = str1[d + i];
+			str1[d + i] = str1[length - 1 - i];
+			str1[length - 1 - i] = temp;
+		}
 	}
 }
