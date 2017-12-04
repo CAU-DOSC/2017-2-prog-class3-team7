92e7286fecd8a9c6ae919b913c5c49d8cad71f0d
galaxy821 <hoshion208@naver.com>
2017-12-05 01:49:05 +0900

    modified juggle.c and header.h

--- Rotation/Header.h
+++ Rotation/Header.h
@@ -9,3 +9,4 @@ void trivial(char *str1, int length, int d);
 void reverse(char *str1, int length, int d);
 void juggle (char *str, int length, int d);
 void BlockSwap(char *str, int n, int d);
+int gcd(int num1, int num2);
--- Rotation/juggle.c
+++ Rotation/juggle.c
@@ -1,39 +1,64 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int gcd(int num1, int num2)
+{
+	int num_gcd;
+	for (int i = 1; i < (num1 > num2 ? num1 : num2); i++)
+	{
+		if ((num1%i == 0) && (num2&i == 0))
+			num_gcd = i;
+	}
+	return num_gcd;
+}
+
 void juggle(char *str1, int length, int d)
 {
-	char* juggle = (char *)malloc(length + 1);
-
+	int i, j, k, temp, g;
 	if (d > 0)
 	{
-		for (int i = 0; i < length; i++)
+		for (i = 0; i < gcd(d, length); i++)
 		{
-			if (i > d - 1)
-				juggle[i - d] = str1[i];
-			else
-				juggle[i + (length - d)] = str1[i];
-		}
-		for (int i = 0; i < length; i++)
-		{
-			str1[i] = juggle[i];
-		}
-		return;
-	}
-	else if (d < 0)
-	{
-		for (int i = 0; i < length; i++)
-		{
-			if (i < (length - (-d)))
-				juggle[i + (-d)] = str1[i];
-			else
-				juggle[i - (length - (-d))] = str1[i];
-		}
-		for (int i = 0; i < length; i++)
-		{
-			str1[i] = juggle[i];
-		}
-		return;
-	}
+			temp = str1[i];
+			j = i;
+			while (1)
+			{
+				k = j + d;
+				if (k >= length)
+					k = k - length;
 
+				if (k == i)
+					break;
+
+				str1[j] = str1[k];
+				j = k;
+			}
+			str1[j] = temp;
+		}
+	}
+	else
+	{
+		d = -d;
+		i = 0;
+		while (i < gcd(d, length))
+		{
+			g = length - 1;
+			temp = str1[g];
+			j = g;
+			while (1)
+			{
+				k = j - d;
+				if (k < 0)
+					k = k + length;
+				if (k == i)
+					break;
+				str1[j] = str1[k];
+				j = k;
+			}
+			str1[j] = temp;
+			i++;
+		}
+	}
+	return;
 }
+
